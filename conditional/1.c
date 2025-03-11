ddb5576238b1d716abc1b9780c8aeafaf580b2c6
lav-kush-garg <lavkushgarg085@gmail.com>
2025-03-11 17:24:59 +0530

    c

--- conditional/1.c
+++ conditional/1.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main() {
-    int a, b;
-    printf("Enter two integers: ");
-    scanf("%d %d", &a, &b);
-
-    if(a == b) {
-        printf("Both numbers are equal.\n");
-    }
-
+    int angle1, angle2, angle3;
+    printf("Enter three angles of a triangle: ");
+    scanf("%d %d %d", &angle1, &angle2, &angle3);
+    if (angle1 + angle2 + angle3 == 180)
+        printf("The angles form a valid triangle\n");
+    else
+        printf("The angles do not form a valid triangle\n");
     return 0;
 }
---
+++ conditional/13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("The number is positive.\n");
+
+        if (num % 2 == 0) {
+            printf("It is even.\n");
+        } else {
+            printf("It is odd.\n");
+        }
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
